c5f6d3a33e757bce696edbf4e67af582da1f5f52
M.Shaheer Khan <160488347+Sherrrrryyy@users.noreply.github.com>
2024-03-09 01:12:50 +0500

    Add files via upload

---
+++ Class 22/app.js
@@ -0,0 +1,393 @@
+// CHAP 01  ALERTS
+
+// alert("Welcome to our Website")
+
+// alert("Error! please enter a valid password")
+
+// alert("Welcome to JS Land \n Happy Coding")
+
+// alert("Welcome to JS Land")
+// alert("Happy Coding")
+
+
+// CHAP 02 VARIABLES FOR STRINGS
+
+// var userName;
+
+// var myName = "M. Shaheer Khan"
+// console.log(myName);
+
+// var message = "Hello World";
+// alert(message);
+
+// var stuName = "Saad Mushtaq";
+// alert(stuName);
+
+// var stuId = "20" + " " + "years old";
+// alert(stuId);
+
+// var stuTech = "Web and Mobile app Development";
+// alert(stuTech);
+
+// var fastFood = "Pizza \nPizz \nPiz \nPi \nP"
+// alert(fastFood);
+
+// var email = "example@gmail.com";
+// alert("My email address is " + email);
+
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book " + book);
+
+
+// var writwHtml = "Yah i can write HTML content through JavaScript"
+// document.getElementById("demo").innerHTML = writwHtml;
+
+
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// document.getElementById("demo2").innerHTML = design;
+// alert(design);
+
+
+// CHAP 03 VARIABLES FOR NUMBERS
+
+// var age = 18;
+// alert("I am" + age + " years old");
+
+// var visit = "14";
+// alert("You have visited the site " + visit + " times");
+
+// var birthYear = 2005;
+// document.getElementById("demos").innerHTML = "My birthyear is " + birthYear;
+
+// document.getElementById("demo1").innerHTML = "Data type of my declared variable is number";
+
+// var visName = "Shaheer";
+// var proTitle = "T-shirts";
+// var proQuan = 10;
+
+// document.getElementById("cloths").innerHTML = visName + " ordered " + proQuan + " " + proTitle + " on XYZ clothing store" 
+
+
+
+// CHAP 04 VARIABLES NAMES: LEGAL & ILLEGAL
+
+// var studentId = 25, studentName = "Daniyal", fatherName = "Aftab";
+// console.log(studentId, studentName, fatherName);
+
+// LEGAL
+
+// var pehlaLegal        
+// var Dusralegal$
+// var TEESRA_legal
+// var $CHOHTALEGAL
+// var _$panchwaLegaL
+
+// ILLEGAL
+
+// var 1ILLEGAL
+// var +ILLEGAL
+// var ?ILLEGAL
+// var /ILLEGAL
+// var @ILLEGAL
+
+
+// document.getElementById("heading").innerHTML = "Rules for naming JS variables"
+
+// var dollar = "$";
+// var under = "_";
+// var numeric = 1;
+// var alphabetic = "stVariable";
+// document.getElementById("headinga").innerHTML = "Variables can only contain " + dollar + under + numeric + alphabetic;
+
+// var firstExample = "$name";
+// var secondExample = "_name";
+// var thirdExample = "name";
+// document.getElementById("headingb").innerHTML = "Variable must begin with a " + firstExample + secondExample + " or " + thirdExample;
+
+// var whatCase = "Case Sensitive";
+// document.getElementById("headingc").innerHTML = "Variables names are " + whatCase
+
+
+// var keyCase = "Keywords";
+// document.getElementById("headingd").innerHTML = "Variables names should not be " + keyCase
+
+
+// CHAP 05 MATH EXPRESSIONS
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 + num2;
+
+// document.getElementById("sum").innerHTML = "Sum of " + num1 + " and " + num2 + " is " + result;
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 - num2;
+
+// document.getElementById("substract").innerHTML = "Substract of " + num1 + " and " + num2 + " is " + result;
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 * num2;
+
+// document.getElementById("multiply").innerHTML = "Multiplication of " + num1 + " and " + num2 + " is " + result;
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 / num2;
+
+// document.getElementById("division").innerHTML = "division of " + num1 + " and " + num2 + " is " + result;
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 % num2;
+
+// document.getElementById("modulus").innerHTML = "modulus of " + num1 + " and " + num2 + " is " + result;
+
+// var number1;
+// number1 = 5
+
+// document.getElementById("exercised").innerHTML = "Initial Value: " + number1;
+// document.getElementById("exercisef").innerHTML = "Value after Increment: " + ++number1;
+
+// number1 = number1 + 7
+// document.getElementById("exerciseg").innerHTML = "Value after addition is: " + number1;
+// document.getElementById("exercisej").innerHTML = "Value after decrement is: " + --number1;
+
+// var result = number1 % 4;
+// document.getElementById("output").innerHTML = "Output: The reamainder is " + result;
+
+
+// var line = "Total cost to buy 5 tickets to a movie is"
+// var ticketPrice1 = 600
+// var ticketPrice2 = 600
+// var ticketPrice3 = 600
+// var ticketPrice4 = 600
+// var ticketPrice5 = 600
+// var totalPrice = ticketPrice1 + ticketPrice2 + ticketPrice3 + ticketPrice4 + ticketPrice5;
+// document.getElementById("ticket").innerHTML =  line + " " + totalPrice;
+
+
+// var tables = "Table of 9"
+// document.getElementById("table").innerHTML = tables;
+
+
+// var tables1 = "9 * 1 ="
+// var table1 = 9
+// var equal1 = 1
+// var result = table1 * equal1;
+// document.getElementById("table1").innerHTML = tables1 + result
+
+// var tables1 = "9 * 1 ="
+// var table1 = 9
+// var equal1 = 1
+// var result = table1 * equal1;
+// document.getElementById("table1").innerHTML = tables1 + result
+
+// var tables1 = "9 * 1 ="
+// var table1 = 9
+// var equal1 = 1
+// var result = table1 * equal1;
+// document.getElementById("table1").innerHTML = tables1 + result
+
+// var tables1 = "9 * 1 ="
+// var table1 = 9
+// var equal1 = 1
+// var result1 = table1 * equal1;
+// document.getElementById("table1").innerHTML = tables1 + result1
+
+// var tables2 = "9 * 2 ="
+// var table2 = 9
+// var equal2 = 2
+// var result2 = table2 * equal2;
+// document.getElementById("table2").innerHTML = tables2 + result2
+
+// var tables3 = "9 * 3 ="
+// var table3 = 9
+// var equal3 = 3
+// var result3 = table3 * equal3;
+// document.getElementById("table3").innerHTML = tables3 + result3
+
+// var tables4 = "9 * 4 ="
+// var table4 = 9
+// var equal4 = 4
+// var result4 = table4 * equal4;
+// document.getElementById("table4").innerHTML = tables4 + result4
+
+// var tables5 = "9 * 5 ="
+// var table5 = 9
+// var equal5 = 5
+// var result5 = table5 * equal5;
+// document.getElementById("table5").innerHTML = tables5 + result5
+
+// var tables6 = "9 * 6 ="
+// var table6 = 9
+// var equal6 = 6
+// var result6 = table6 * equal6;
+// document.getElementById("table6").innerHTML = tables6 + result6
+
+// var tables7 = "9 * 7 ="
+// var table7 = 9
+// var equal7 = 7
+// var result7 = table7 * equal7;
+// document.getElementById("table7").innerHTML = tables7 + result7
+
+// var tables8 = "9 * 8 ="
+// var table8 = 9
+// var equal8 = 8
+// var result8 = table8 * equal8;
+// document.getElementById("table8").innerHTML = tables8 + result8
+
+// var tables9 = "9 * 9 ="
+// var table9 = 9
+// var equal9 = 9
+// var result9 = table9 * equal9;
+// document.getElementById("table9").innerHTML = tables9 + result9
+
+// var tables10 = "9 * 10 ="
+// var table10 = 9
+// var equal10 = 10
+// var result10 = table10 * equal10;
+// document.getElementById("table10").innerHTML = tables10 + result10
+
+// var celcius = 15
+// var farenheit = (celcius * 9/5) + 32
+// document.getElementById("conversion1").innerHTML = celcius + " is " + farenheit;
+
+// var farenheit1 = 36
+// var celcius1 = (farenheit1 - 32) * 5/9;
+// document.getElementById("conversion2").innerHTML = farenheit1 + " is " + celcius1;
+
+
+// var headingHead = "SHOPPING CART"
+// document.getElementById("quanhead").innerHTML = headingHead
+
+
+// var line1 = "Price of item 1 is 650"
+// document.getElementById("price1").innerHTML = line1
+
+// var line2 = "quantity of item 1 is 3"
+// document.getElementById("quan1").innerHTML = line2
+
+// var line3 = "Price of item 2 is 100"
+// document.getElementById("price2").innerHTML = line3
+
+// var line4 = "quantity of item 2 is 7"
+// document.getElementById("quan2").innerHTML = line4
+
+// var line5 = "Shipping Charges 100"
+// document.getElementById("shipcharge").innerHTML = line5
+
+
+// var price1 = 650
+// var quantityItem = 3
+// var finalPrice = price1 * quantityItem
+
+// var price2 = 100
+// var quantityItem1 = 7
+// var finalPrice1 = price2 * quantityItem1
+
+// var shippingCharge = 100
+// var finalCharges = finalPrice + finalPrice1 + shippingCharge
+
+// document.getElementById("finalcharge").innerHTML = "Total cost of your order is " + finalCharges;
+
+// markSheet = "MARKSHEET"
+// document.getElementById("marksheet").innerHTML = markSheet
+
+
+// var totalMarks = 980
+// document.getElementById("marksline").innerHTML = "Total Marks:" + " " + totalMarks
+
+// var marksObt = 804
+// document.getElementById("obtainline").innerHTML = "Obtain Marks: " + marksObt
+
+
+// var percentage = (totalMarks - marksObt)/(marksObt)*100
+// console.log(percentage)
+
+
+// document.getElementById("currencyhead").innerHTML = "Currency in PKR"
+
+
+// var dollar = 10
+// var riyal = 25
+// var dollarPkr = 104.80
+// var riyalPkr = 28
+// var pakistaniRupee = (dollar * dollarPkr) + (riyal * riyalPkr)
+
+// document.getElementById("totalcurrency").innerHTML = "Total currency in PKR: " + pakistaniRupee
+
+// randomNumber = 47
+// FirstResult = ((randomNumber + 5) * 10) / 2
+// console.log(FirstResult)
+
+
+// document.getElementById("age").innerHTML = "Age Calculator"
+// var currYear = 2024
+// var birthYear = 1971
+// var finalYear = currYear - birthYear
+// document.getElementById("finalage").innerHTML = "Your age is: " + finalYear
+// console.log(finalYear)
+
+
+// document.getElementById("geom").innerHTML = "The Geometrize"
+
+// var radius = 20
+// var circumference = 2 * 3.142 * radius
+// var area = 3.142 * (radius ** 2)
+
+// document.getElementById("radius").innerHTML = "Radius of a circle is: " + radius
+// document.getElementById("circumfrence").innerHTML = "The circumfrences is: " + circumference
+// document.getElementById("area").innerHTML = "The area is: " + area 
+
+
+// document.getElementById("lifetime").innerHTML = "The Lifetime Supply Calculator"
+
+// var favSnack = "Tuc"
+// var currAge = 18
+// var maxAge = 75
+// var estiAmount = 4
+// var remYears = maxAge - currAge
+// var totalAmount = remYears * estiAmount
+
+// document.getElementById("snack").innerHTML = "Favourite Snack " + favSnack
+// document.getElementById("age").innerHTML = "Current Age " + currAge
+// document.getElementById("Mage").innerHTML = "Estimated Maximum Age " + maxAge
+// document.getElementById("perday").innerHTML = "Amount of snacks per day " + estiAmount
+// document.getElementById("finale").innerHTML = "You will need " + totalAmount + " " + favSnack + " " + "to last you until the ripe old age of " + maxAge 
+// console.log(totalAmount)
+
+
+// CHAP 6-9 ALERTS
+
+
+document.getElementById("result").innerHTML = "RESULT"
+
+var a = 10
+document.getElementById("number1").innerHTML = "The value of number1 is: " + a 
+
+var b = ++a 
+document.getElementById("number2").innerHTML = "The value of ++a is: " + b
+document.getElementById("number2a").innerHTML = "Now the value of a is: " + b 
+
+var c = ++a
+document.getElementById("number3").innerHTML = "The value of ++a is: " + b
+document.getElementById("number3a").innerHTML = "Now the value of a is: " + c
+
+var d = --a
+document.getElementById("number4").innerHTML = "The value of --a is: " + d
+document.getElementById("number4a").innerHTML = "Now the value of a is: " + d
+
+
+var e = a--
+document.getElementById("number5").innerHTML = "The value of a-- is: " + e
+document.getElementById("number5a").innerHTML = "Now the value of a is: " + e
+
+
+
+ 
